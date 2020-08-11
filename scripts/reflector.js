17f249480b56907fb4cee2eaec031a7c94da26c4
Solokil <43015094+Solokil@users.noreply.github.com>
2020-08-11 18:16:19 -0300

    Update reflector.js

--- scripts/reflector.js
+++ scripts/reflector.js
@@ -1,4 +1,4 @@
-const refle = extendContent(Unit, "advance-alloy-wall-large", {
+const refle = extendContent(Unit, "reflector", {
 	handleBulletHit(entity, bullet){
 		this.super$handleBulletHit(entity, bullet);
 		
@@ -6,12 +6,12 @@ const refle = extendContent(Unit, "advance-alloy-wall-large", {
 		
 		vec.trns(Angles.angle(entity.x, entity.y, bullet.x, bullet.y), this.size * Vars.tilesize / 2);
 		
-		if(Mathf.chance(this.lightningChance)){
-			Lightning.create(entity.getTeam(), Color.valueOf("a9d8ff"), this.lightningDamage, vec.x + entity.x, vec.y + entity.y, bullet.rot() + 180, this.lightningLength + Mathf.random(0, 4));
+		if(Mathf.chance(this.reflectChance)){
+			Bullet.create(entity.getTeam(), Color.valueOf("a9d8ff"), this.lightningDamage, vec.x + entity.x, vec.y + entity.y, bullet.rot() + 180, this.lightningLength + Mathf.random(0, 4));
 		}
 	}
 });
 
-refle.lightningChance = 0.1;
+refle.reflectChance = 0.1;
 refle.lightningDamage = 17;
 refle.lightningLength = 15;
