fd3d73e3c505c4a5e76a55f574ac22db97a970f3
Solokil <43015094+Solokil@users.noreply.github.com>
2020-08-11 20:29:57 -0300

    Update reflector.js

--- scripts/reflector.js
+++ scripts/reflector.js
@@ -4,7 +4,7 @@ const refle = extendContent(UnitType, "reflector", {
 		
 		const vec = new Vec2();
 		
-		vec.trns(Angles.angle(entity.x, entity.y, bullet.x, bullet.y), Vars.tilesize / 2);
+		vec.trns(Angles.angle(entity.x, entity.y, bullet.x, bullet.y), 1);
 		
 		if(Mathf.chance(this.reflectChance)){
 			Lightning.create(entity.getTeam(), Color.valueOf(bullet.frontColor), bullet.damage, vec.x + entity.x, vec.y + entity.y, bullet.rot() + 180, this.lightningLength + Mathf.random(0, 4));
