13078b47e6b445b0483b0fbe3d7941d34216bb26
Solokil <43015094+Solokil@users.noreply.github.com>
2020-09-30 22:07:20 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 if(typeof(require)  !== "undefined")
 {
-   require("mass-destroyer-worm");
+   // require("mass-destroyer-worm");
+   require("scourge");
    // require("auto-he");
    require("funclib");
 
